73644b0503d10293e37829e292c15e641625174d
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2026-07-04 02:10:26 +0800

    [day-7] LongestRepeatingChar solution

--- app/src/main/kotlin/sliding_window/LongestRepeatingChar.kt
+++ app/src/main/kotlin/sliding_window/LongestRepeatingChar.kt
@@ -2,16 +2,26 @@ package sliding_window
 
 class LongestRepeatingChar {
     fun characterReplacement(s: String, k: Int): Int {
-        var total = 0
+        var maxWindow = 0
+        var answer = 0
         var left = 0
-        var arr = IntArray(26)
+        var map = hashMapOf<Char, Int>()
         if (s.isEmpty()) return 0
         if (s.length == 1) return 1
         for (right in s.indices) {
+            val ch = s[right]
+            map[ch] = map.getOrDefault(ch, 0) + 1
+            maxWindow = maxOf(maxWindow, map[ch]!!)
 
+            while ((right - left + 1) - maxWindow > k) {
+                val leftCh = s[left]
+                map[leftCh] = map[leftCh]!! -1
+                left++
+            }
+
+            answer = maxOf(answer, right - left + 1)
         }
-
-        return total
+        return answer
 
     }
 }
