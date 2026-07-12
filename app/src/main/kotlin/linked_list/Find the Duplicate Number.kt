b14800af8638fad420326056f7b9aa1b9a6e0f8f
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2026-07-12 16:59:36 +0800

    [day-14] Find the Duplicate Number

---
+++ app/src/main/kotlin/linked_list/Find the Duplicate Number.kt
@@ -0,0 +1,26 @@
+package linked_list
+
+class `Find the Duplicate Number` {
+
+    fun findDuplicate(nums: IntArray): Int {
+
+        var slow  = nums[0]
+        var fast = nums[0]
+
+        do {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        } while (slow != fast)
+
+        var ptr1 = slow
+        var ptr2 = nums[0]
+
+        while(ptr1 != ptr2) {
+            ptr1 = nums[ptr1]
+            ptr2 = nums[ptr2]
+        }
+
+        return ptr2
+
+    }
+}
