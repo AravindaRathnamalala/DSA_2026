c4f3b9bea15dbb9431f94e65471937ee6d26ff78
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2026-07-18 10:48:34 +0800

    [day-19] BinarySearch implementation.

---
+++ app/src/main/kotlin/algo/BinarySearch.kt
@@ -0,0 +1,30 @@
+package algo
+
+class BinarySearch {
+
+    fun binarySearch(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right  = nums.size-1
+        while (left <= right) {
+            val mid = left +(right-left)/2
+
+            when{
+                nums[mid] == target -> return mid
+                nums[mid] > target -> left = mid+1
+                else -> right = mid-1
+            }
+        }
+        return -1
+    }
+
+
+    fun binarySearchWithRecursion(nums: IntArray, target: Int, left: Int, right: Int) : Int {
+        if (left >= right) return -1
+        val mid  = left+ (left - right)/2
+        return when{
+            nums[mid] == target -> mid
+            nums[mid] > target -> binarySearchWithRecursion(nums, target, mid + 1, right)
+            else -> binarySearchWithRecursion(nums, target, mid - 1, right)
+        }
+    }
+}
